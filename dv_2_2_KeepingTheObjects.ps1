a10ac0e4428a1c36dcbe7562ccfe47aaa5dae2a4
Dhruv Vashistha <dhruv.vashistha@ssdntech.co.in>
2026-07-26 04:39:28 +0000

    changing second video

--- dv_2_2_KeepingTheObjects.ps1
+++ dv_2_2_KeepingTheObjects.ps1
@@ -1,3 +1,11 @@
 Get-Process a* |
 Get-Member |
 Select-Object -Property MemberType -Unique
+
+#the below command is invalid by the way:
+#Get-Process a* | Get-Member -Property MemberType | Select-Object -Unique
+#get-member contain multiple things like properties, methods, events, etc. and the -Property parameter is not valid for Get-Member.
+#get-process w* | Get-Member -Type AliasProperty | Select-Object -Property Name
+
+write-output "this is where things get started"
+Get-Process a* | Get-Member -Type AliasProperty | Select-Object -Property Name
