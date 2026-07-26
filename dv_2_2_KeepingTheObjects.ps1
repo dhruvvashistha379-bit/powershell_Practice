aa9fb5dd3c09d4a9c07022ef93faefbe82c74ce9
Dhruv Vashistha <dhruv.vashistha@ssdntech.co.in>
2026-07-26 05:33:14 +0000

    adding

--- dv_2_2_KeepingTheObjects.ps1
+++ dv_2_2_KeepingTheObjects.ps1
@@ -1,14 +1,13 @@
-$PSStyle.OutputRendering = 'PlainText'
 Get-Process a* |
 Get-Member |
-Select-Object -Property MemberType -Unique
+Select-Object -Property MemberType -Unique;
 
-#the below command is invalid by the way:
+#the below command is invalid by the way->
 #Get-Process a* | Get-Member -Property MemberType | Select-Object -Unique
-#get-member contain multiple things like properties, methods, events, etc. and the -Property parameter is not valid for Get-Member.
+#get-member contain multiple things like properties, methods, events, etc. and the -Property parameter is not valid for Get-Member
 #get-process w* | Get-Member -Type AliasProperty | Select-Object -Property Name
 
-write-output "this is where things get started"; Get-Process a* | Get-Member -Type AliasProperty | Select-Object -Property Name
-#paste above in powershell 7 and get the output there, seems something is wrong with access on this vdi.
+write-output "this is where things get started"; Get-Process a* | Get-Member -Type AliasProperty | Select-Object -Property Name;
+#paste above in powershell 7 and get the output there, seems something is wrong with access on this vdi
 
-get-process | where-object {$_.name -eq "powershell"} | Get-Member  #could have used gm only instead of Get-Member
+get-process | where-object {$_.name -eq "powershell"} | Get-Member;  #could have used gm only instead of Get-Member
